06ee1998902b881e691525db0e4af3d2adff4aa7
Fahadjalil7 <fkhan368@uwo.ca>
2025-03-18 04:58:05 -0400

    updated to include  Db file

--- routes/auth.js
+++ routes/auth.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const { getDB } = require("../../DB/DB1");
+const { getDB } = require("../DB1");
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 require("dotenv").config();
